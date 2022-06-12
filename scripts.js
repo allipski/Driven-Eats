c915786afcfc7265a5220834814c3111fd6fc053
Alipski <alicematobalipski@gmail.com>
2022-06-12 15:21:54 -0300

    logica para mudar a cor do botao ja esta implementada, so falta gerar o link do whatsapp com a mensagem

--- scripts.js
+++ scripts.js
@@ -1,7 +1,47 @@
-function selectitem(elemento) {
-    const jaTem = document.getElementById("comida").querySelector("itemselecionado");
-    if (jaTem === null){
-        jaTem.classList.add("itemselecionado");
-    } 
+function fecharPedido() {
+    const podeComprar = document.querySelectorAll(".itemselecionado").length;
+    if (podeComprar === 3) {
+    const botao = document.getElementById("botao");
+    botao.innerHTML = "Fechar Pedido";
+    botao.classList.add("botaoverde");
+    }
+}
 
+function selectitem1(elemento) {
+    const divPai = document.getElementById("comida");
+    const divFilha = divPai.querySelector(".itemselecionado");
+    if (divFilha!==null){
+        divFilha.classList.remove("itemselecionado");
+        elemento.classList.add("itemselecionado");
+    }
+    else { 
+        elemento.classList.add("itemselecionado");
+    }
+    fecharPedido();
+}
+
+function selectitem2(elemento) {
+    const divPai = document.getElementById("bebida");
+    const divFilha = divPai.querySelector(".itemselecionado");
+    if (divFilha!==null){
+        divFilha.classList.remove("itemselecionado");
+        elemento.classList.add("itemselecionado");
+    }
+    else {
+        elemento.classList.add("itemselecionado");
+    }
+    fecharPedido();
+}
+
+function selectitem3(elemento) {
+    const divPai = document.getElementById("sobremesa");
+    const divFilha = divPai.querySelector(".itemselecionado");
+    if (divFilha!==null){
+        divFilha.classList.remove("itemselecionado");
+        elemento.classList.add("itemselecionado");
+    }
+    else {
+        elemento.classList.add("itemselecionado");
+    }
+    fecharPedido();
 }
